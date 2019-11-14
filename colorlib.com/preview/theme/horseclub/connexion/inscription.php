1427135c793851031c0d55bc6093c7c1fda01c23
NiXyos <sen.ellena.n@gmail.com>
2019-11-14 09:23:01 +0100

    revient sur connexion

--- colorlib.com/preview/theme/horseclub/connexion/inscription.php
+++ colorlib.com/preview/theme/horseclub/connexion/inscription.php
@@ -5,7 +5,7 @@
 
     // S'il y a une session alors on ne retourne plus sur cette page
     if (isset($_SESSION['id'])){
-        header('Location: index.php');
+        header('Location:connexion.php');
         exit;
     }
 
