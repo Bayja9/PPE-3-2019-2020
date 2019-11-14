efb91f06ecec3bc512b69d1ff970c07574689b06
NiXyos <sen.ellena.n@gmail.com>
2019-11-14 09:20:18 +0100

    bdd

--- colorlib.com/preview/theme/horseclub/connexion/connexionDB.php
+++ colorlib.com/preview/theme/horseclub/connexion/connexionDB.php
@@ -1,8 +1,8 @@
 <?php
-// Déclaration d'une nouvelle classe
+// DÃ©claration d'une nouvelle classe
 class connexionDB {
     private $host    = 'localhost';    // nom de l'host
-    private $name    = 'connexion';     // nom de la base de donnée
+    private $name    = 'centre_equestre';     // nom de la base de donnï¿½e
     private $user    = 'root';         // utilisateur
     private $pass    = '';         // mot de passe
     //private $pass    = '';           // Ne rien mettre si on est sous windows
@@ -10,17 +10,17 @@ class connexionDB {
 
     function __construct($host = null, $name = null, $user = null, $pass = null){
         if($host != null){
-            $this->host = $host;           
-            $this->name = $name;           
-            $this->user = $user;          
+            $this->host = $host;
+            $this->name = $name;
+            $this->user = $user;
             $this->pass = $pass;
         }
     try{
         $this->connexion = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->name,
-            $this->user, $this->pass, array(PDO::MYSQL_ATTR_INIT_COMMAND =>'SET NAMES UTF8', 
+            $this->user, $this->pass, array(PDO::MYSQL_ATTR_INIT_COMMAND =>'SET NAMES UTF8',
             PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
     }catch (PDOException $e){
-        echo 'Erreur : Impossible de se connecter  à la BDD !';
+        echo 'Erreur : Impossible de se connecter  ï¿½ la BDD !';
     die();
    }
   }
@@ -36,6 +36,6 @@ class connexionDB {
     }
 }
 
-// Faire une connexion à votre fonction
+// Faire une connexion ï¿½ votre fonction
 $DB = new connexionDB();
 ?>
